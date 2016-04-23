b9ec777af1e3ddc58b85a270cfeb97b465a93be3
oxpeter <oxpeter+git@gmail.com>
2016-04-22 23:23:51 -0400

    added gff file creation

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -7,6 +7,7 @@ detail the phylogenetic relationships between all closely related proteins.
 
 import argparse
 import datetime
+import itertools
 import os
 import re
 import tempfile
@@ -92,7 +93,11 @@ class Gene_family(object):
     different criteria. Primarily this class allows fast indexing of all the
     transcript members, criteria members, and merging of two gene_families.
     """
+    _ids = itertools.count(0) # to give a unique id to each instance
+
     def __init__(self, transcript_list):
+        self.id = self._ids.next()
+
         self.members = { t:True for t in transcript_list }
         self.memberid = { t.trinity_id:True for t in transcript_list }
         self.blast_hits = { t.blast:True for t in transcript_list if t.blast is not None }
@@ -137,9 +142,6 @@ class Gene_family(object):
         for t in gf.members:
             self.add_transcript(t)
 
-
-
-
 ############################################################################
 
 def define_arguments():
@@ -163,9 +165,17 @@ def define_arguments():
                         (eg Trinity.fasta.transdecoder.pep) to analyse""")
     parser.add_argument("-f", "--fasta", type=str,
                         help="""fasta file (eg Trinity.fasta) to analyse""")
+    parser.add_argument("-g", "--gff", type=str,
+                        help="""gff file to enable new gff to be constructed from
+                        combined gene families.""")
     parser.add_argument("-b", "--blast", type=str,
                         help="""blast results file (outfmt 6) to assign genes to
                         transcripts""")
+    parser.add_argument("-S", "--stats", action='store_true',
+                        help="""perform basic stats on the fasta file (N50 etc)""")
+    parser.add_argument("-L", "--longest", action='store_true',
+                        help="""extract the longest transcript from each gene, and
+                        save a fasta file of the peptide sequences.""")
 
     return parser
 
@@ -186,22 +196,53 @@ def report(gene_families, num=5, handle=None, verbalise=lambda *x:None):
 
     verbalise("G", "%d transcripts in %d gene families" % (sum(len(gf) for gf in gene_families),
                                                 len(gene_families) ))
-    verbalise("Y",
+    verbalise("G",
         "%d gene families with >1 member" % (sum(1 for gf in gene_families if len(gf) > 1)))
-    verbalise("Y",
+    verbalise("G",
         "%d gene families with >1 blast result" % (sum(1 for gf in gene_families if len(gf.blast_hits) > 1)))
 
     print ""
 
     # sanity check output:
-    for gf in gene_families.keys()[:num]:
+    for gcount, gf in enumerate(gene_families.keys()[:num]):
         if handle:
             handle.write("%s\n" % gf)
         verbalise("M", gf)
         for t in gf:
             if handle:
-                handle.write("%s\n" % t)
+                handle.write("%d %s\n" % (gcount, t))
             verbalise("B", t)
+
+def make_gff(outhandle, gff, gene_families, gf_idx):
+    no_family = {}
+    no_transcript = {}
+    handle = open(gff, 'rb')
+    for line in handle:
+        cols = line.split()
+        if len(cols) >= 8:
+            fields = cols[:8]
+            atts = " ".join(cols[8:])
+            if fields[2] == 'exon': # exon includes five_prime_utr, cds, and three_prime_utr
+                ts_s = re.search('ID=([\w\|\.]+).exon\d+;', atts)
+                if ts_s:
+                    ts = ts_s.group(1)
+                    if ts in gf_idx:
+                        gf = gf_idx[ts]
+                        newatts = ['gene_id "SGF%d"; transcript_id "%s";\n' % (gf.id, ts)]
+                        newline = '\t'.join(fields + newatts)
+                        outhandle.write(newline)
+                    else:
+                        no_family[ts_s.group(1)] = line.strip()
+                else:
+                    no_transcript[line.strip()] = line.strip()
+
+    if len(no_family) > 0:
+        verbalise("M", "%d transcripts found in gff that are not in the gf_idx" % len(no_family))
+        verbalise("R", "\n".join(str(k)+" "+str(v) for k,v in no_family.items()[:5]))
+    if len(no_transcript)>0:
+        verbalise("M", "%d transcripts from gff not matched by regex profile" % len(no_transcript))
+
+
 
 def progress(counter, total, t0):
     counter += 1
@@ -249,7 +290,29 @@ def to_edges(l):
         yield last, current
         last = current
 
+def fasta_stats(fastafile, display=False):
+    # get basic stats on the fasta file:
+    genelens = sorted( [ len(seq[1]) for seq in internal.parsefasta(fastafile)] , reverse=True)
+    totallen = sum(genelens)
 
+    print "Number of transcripts: %d" % len(genelens)
+    print "Total length of transcripts: %d" % totallen
+    print "longest transcript: %d" % genelens[0]
+
+    if display:
+        plt.hist(genelens)
+        plt.title("Histogram of gene lengths (bp)")
+        plt.show()
+    prevl = 0
+    lensum = 0
+    trancount = 0
+    for l in genelens:
+        if lensum + l >= (totallen / 2):
+            print "N50: %d (%d transcripts)" % (prevl, trancount)
+            break
+        lensum += l
+        prevl = l
+        trancount += 1
 
 if __name__ == '__main__':
     dbpaths = config.import_paths()
@@ -263,7 +326,12 @@ if __name__ == '__main__':
     temp_dir = tempfile.mkdtemp()
     os.rmdir(temp_dir)  # dir must be empty!
 
-    # collate genes with identical sequences:
+    # if requested, report the basic stats about the supplied fasta file:
+    if args.stats and args.fasta:
+        fasta_stats(args.fasta, args.display_on)
+
+    #############################################################################
+    ### extract sequence information from each gene in supplied transdecoder file
     transcript_dic = {}
     gene_families = {}
     seq_families = {}
@@ -304,7 +372,8 @@ if __name__ == '__main__':
 
     verbalise("Y", "%d trinity gene ids\n\n" % (len(geneid_idx)))
 
-    # assemble gene_families starting with sequence similarity
+    ############################################################
+    ### assemble gene_families starting with sequence similarity
     for ts in seq_families.values():
         newfamily = Gene_family(ts)
 
@@ -315,15 +384,16 @@ if __name__ == '__main__':
                 gf_idx[t].append(newfamily)
             else:
                 gf_idx[t] = [newfamily]
-
+    verbalise("Y", "Results after merging based on sequence identity:")
     report(gene_families, 5, verbalise=verbalise)
 
-
-    # merge solution from SO:
+    #############################################################################
+    # merge gene families based on Trinity gene groups (solution from SO):
     G = to_graph(gene_families)
 
     # collect new gene families:
     trinity_pool = {}
+    gf_idx = {} # reset the gene family index to the new groups
     count = 0
 
     for group in connected_components(G):
@@ -332,50 +402,38 @@ if __name__ == '__main__':
         for gid in group:
             ts += geneid_idx[gid]
         newgf = Gene_family(ts)
+
+        # set index to find gf for given transcript:
+        for t in newgf:
+            gf_idx[t.td_id] = newgf
+
         trinity_pool[newgf] = True
 
-    print count, "\n\nmerged families from networkx"
+    # report results:
+    verbalise("Y", "\n\nResults after merging based on Trinity gene assignment:")
     report(trinity_pool, 5, verbalise=verbalise)
 
-    """
-    # merge families based on trinity gene name:
-    trinity_pool = {} # index on members to removed duplicate genefamilies
-    verbalise("B", "\nMerging datasets")
-    total = len(gene_families)
-    counter = 0
-    t0 = datetime.datetime.now()
-    for gf in gene_families.keys():
-        for gid in gf.geneids.keys():
-            if gid in geneid_idx:
-                for t in geneid_idx[gid]:
-                    if t in gf_idx:
-                        for mgf in gf_idx[t]:
-                            if mgf == gf:
-                                pass
-                            else:
-                                gf.merge(mgf)
-
-
-        trinity_pool[tuple(gf.members)] = gf
-        counter = progress(counter, total, t0)
-
-    print ""
-    trinity_dic = { v:True for v in trinity_pool.values()  }
-
-    report(trinity_dic, 5, verbalise=verbalise)
-    """
-
+    ##############################
     # write gene families to file:
-    outhandle = open(logfile[:-3] + "families.out", 'w')
-    report(trinity_pool, None, outhandle)
-    outhandle.close()
+    if args.gff:
+        outhandle = open(logfile[:-3] + "families.gtf", 'w')
+        make_gff(outhandle, args.gff, trinity_pool, gf_idx)
+        outhandle.close()
 
-    plt.hist([len(gf) for gf in trinity_pool], bins=range(12))
-    plt.title("Number of genes in each gene family")
-    plt.ylabel("Number of families")
-    plt.xlabel("Number of trinity genes")
-    plt.savefig(logfile[:-3] + "family_size.pdf", format='pdf')
-    plt.show()
+    if args.display_on:
+        plt.hist([len(gf) for gf in trinity_pool], bins=range(12))
+        plt.title("Number of genes in each gene family")
+        plt.ylabel("Number of families")
+        plt.xlabel("Number of trinity genes")
+        plt.savefig(logfile[:-3] + "family_size.pdf", format='pdf')
+        plt.show()
+
+
+    #############################################################################
+    # get longest peptide for each gene (not necessarily the longest transcript)
+    if args.longest:
+
+
 
 
 
